385f228552062783ce7284ff642267fbc77f01a5
svs8 <surakshithvittalshetty@gmail.com>
2022-04-26 22:23:37 +0530

    Created logic to find whether the number is even or Odd

---
+++ Even_Odd.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz;
+
+public class Even_Odd {
+    static int a=1;
+
+    public static void main(String[] args) {
+        if(a%2==0)
+        {
+            System.out.println("the number "+a+" is Even");
+        }
+        else{
+            System.out.println("The number " +a+" is odd");
+        }
+    }
+}
