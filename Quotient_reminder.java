fcd536134ebeb80faf5a02239b1bff3a0fa8b485
svs8 <surakshithvittalshetty@gmail.com>
2022-04-26 22:30:25 +0530

    Created logic to find the reminder and Quotient of two numbers

---
+++ Quotient_reminder.java
@@ -0,0 +1,18 @@
+//Java Program to Compute Quotient and Remainder
+
+package com.Bridgelabz;
+
+public class Quotient_reminder {
+
+    public static void main(String[] args) {
+        int num1=15;
+        int num2=2;
+        System.out.println("the value of num1 is "+num1);
+        System.out.println("the value of num2 is "+num2);
+        int reminder=num1%num2;
+        int quotient=num1/num2;
+        System.out.println("the reminder is "+reminder);
+        System.out.println("the Quotient is "+quotient);
+
+    }
+}
